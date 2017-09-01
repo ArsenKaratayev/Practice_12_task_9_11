0d6555bbf036715ea2cfaeb33edce22fe209421d
Maria <Maria@LAPTOP-N156UH30>
2017-09-01 18:07:53 +0500

    Добавлена функция, строящая сортировочное дерево (пока не протестирована)

--- Practice_12_task_9_11/Program.cs
+++ Practice_12_task_9_11/Program.cs
@@ -36,18 +36,37 @@ namespace Practice_12_task_9_11
             Console.WriteLine();
         }
 
-        static int[,] MakeSortedTree(int[] mas, int elem, int maxElem)
+        static int[] MakeSortedTree(int[] mas, int maxElem, int elem)
         {//функция, строящая сортировочное дерево
             int maxDescendant = elem;//индекс максимального из двух потомков данного элемента
-            int LeftDescendant=elem*2+1;//индекс левого потомка данного элемента
-            int RightDescendant=elem*2+2;//индекс правого потомка данного элемента
+            int LeftDescendant;
+            int RightDescendant;
             do
             {
-                if (mas[LeftDescendant] > mas[RightDescendant]) maxDescendant = LeftDescendant;
+                LeftDescendant = elem * 2 + 1;//индекс левого потомка данного элемента
+                RightDescendant = elem * 2 + 2;//индекс правого потомка данного элемента
+                if (RightDescendant >= maxElem) maxDescendant = LeftDescendant;//если мы дошли до последнего элемента в неотсортированной части и правый потомок уже в отсортированной части
+                else//если мы еще не дошли до конца неотсортированной части и правый потомок тоже находится в ней
+                if (mas[LeftDescendant] > mas[RightDescendant]) maxDescendant = LeftDescendant;//находим максимальный элемент и  записываем его индекс в maxDescendant
                 else maxDescendant = RightDescendant;
 
-            } while (LeftDescendant != maxElem);
+                if (mas[maxDescendant] <= mas[elem]) break;//если потомок не больше данного элемента, то заканчивам построение сортировочного дерева
+                else//если потомок больше данного элемента, то меняем их местами
+                {
+                    int k = mas[elem];//вспомогательна переменная
+                    mas[elem] = mas[maxDescendant];
+                    mas[maxDescendant] = k;
+                    elem = maxDescendant;//данный элемент меняет индекс
+                }
+            } while (LeftDescendant < maxElem);//пока не дойдем до границы неотсортированной части массива
+            return mas;
         }
+
+        static void PyramidalSorting(int[] mas)
+        {//функция, в которой выполняется два этапа пирамидальной сортировки
+
+        }
+
         static void Main(string[] args)
         {
             //int[] mas = { 4, 6, 7, 10, 7, 0, 9 };

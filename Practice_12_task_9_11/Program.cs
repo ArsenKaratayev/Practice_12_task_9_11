285a7d086058263f40b5de85dfbb38aa682c87de
Maria <Maria@LAPTOP-N156UH30>
2017-09-01 19:40:50 +0500

    была добавлена процедура пирамидальной сортировки и исправлена ошибка в функции, строющей дерево. Цикл с постусловием был заменен на цикл с предусловием, так как при завходе в последний раз он не работал

--- Practice_12_task_9_11/Program.cs
+++ Practice_12_task_9_11/Program.cs
@@ -8,6 +8,14 @@ namespace Practice_12_task_9_11
 {
     class Program
     {
+        static void WriteMas(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+        }
         static void SimpleChoiceSorting(int[] mas)
         {//сортировка простым выбором
             int min;//минимальный элемент в неотсортированной части массива
@@ -29,22 +37,17 @@ namespace Practice_12_task_9_11
                 mas[mini] = k;
             }
             Console.WriteLine("Массив, отсортированный с методом простого выбора: ");
-            for (int i=0; i<mas.Length; i++)
-            {
-                Console.Write(mas[i] + " ");
-            }
-            Console.WriteLine();
+            WriteMas(mas);
         }
 
         static int[] MakeSortedTree(int[] mas, int maxElem, int elem)
         {//функция, строящая сортировочное дерево
             int maxDescendant = elem;//индекс максимального из двух потомков данного элемента
-            int LeftDescendant;
-            int RightDescendant;
-            do
+            int LeftDescendant = elem * 2 + 1;//индекс левого потомка данного элемента
+            int RightDescendant = elem * 2 + 2;//индекс правого потомка данного элемента
+
+            while (LeftDescendant < maxElem)//пока не дойдем до границы неотсортированной части массива
             {
-                LeftDescendant = elem * 2 + 1;//индекс левого потомка данного элемента
-                RightDescendant = elem * 2 + 2;//индекс правого потомка данного элемента
                 if (RightDescendant >= maxElem) maxDescendant = LeftDescendant;//если мы дошли до последнего элемента в неотсортированной части и правый потомок уже в отсортированной части
                 else//если мы еще не дошли до конца неотсортированной части и правый потомок тоже находится в ней
                 if (mas[LeftDescendant] > mas[RightDescendant]) maxDescendant = LeftDescendant;//находим максимальный элемент и  записываем его индекс в maxDescendant
@@ -57,20 +60,40 @@ namespace Practice_12_task_9_11
                     mas[elem] = mas[maxDescendant];
                     mas[maxDescendant] = k;
                     elem = maxDescendant;//данный элемент меняет индекс
+                    LeftDescendant = elem * 2 + 1;//индекс левого потомка данного элемента
+                    RightDescendant = elem * 2 + 2;//индекс правого потомка данного элемента
                 }
-            } while (LeftDescendant < maxElem);//пока не дойдем до границы неотсортированной части массива
+            }
             return mas;
         }
 
         static void PyramidalSorting(int[] mas)
         {//функция, в которой выполняется два этапа пирамидальной сортировки
 
+            //первый этап: составляем пирамиду, прогоняя по не ней элементы, имеющие потомков, начиная с самого нижнего
+            for (int i = mas.Length / 2 - 1; i >= 0; i--)
+                mas = MakeSortedTree(mas, mas.Length, i);//нижней границы пока нет, так как массив еще не начали сортировать
+            WriteMas(mas);
+
+            //второй этап: меняем местами первый и последний в неотсортированной части, затем прогоняем новый верхний элемент, составляя пирамиду
+            for (int i=mas.Length-1; i>=1; i--)
+            {
+                //меняем местами верхний и нижний элементы неотсортированной части (последний элемент - новый край отсортированной части)
+                int k = mas[i];
+                mas[i] = mas[0];
+                mas[0] = k;
+                //составляем пирамиду, передвинув нижний край на один влево. Прогоняем по ней верхний элемент
+                mas = MakeSortedTree(mas, i, 0);
+                WriteMas(mas);
+            }
+            Console.WriteLine("Массив, отсортированный с помощью пирамидальной сортировки:");
         }
 
         static void Main(string[] args)
         {
             //int[] mas = { 4, 6, 7, 10, 7, 0, 9 };
-            //SimpleChoiceSorting(mas);
+            ////SimpleChoiceSorting(mas);
+            //PyramidalSorting(mas);
             //Console.ReadLine();
         }
     }
